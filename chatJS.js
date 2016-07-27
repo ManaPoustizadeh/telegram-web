16f2812a5b4dc8f4c876199d68baab6560ec9cea
MrOlaf <rajabiseraji@outlook.com>
2016-07-28 00:15:47 +0430

    typeBox added

--- chatJS.js
+++ chatJS.js
@@ -1,3 +1,30 @@
 /**
  * Created by mrolaf on 7/27/16.
  */
+
+
+$('.typeBox').click(function(){
+    $(this).toggleClass('selected');
+})
+
+.blur(function(){
+  $(this).toggleClass('selected');
+});
+
+
+
+$(window).resize(function(){
+    if(screen.width/window.innerWidth > 2.1){
+       if(screen.width/window.innerWidth > 2.6){
+           $('.typeBox').removeClass("col-xs-10");
+           $('.typeBox').addClass("col-xs-4");
+       }
+        $('.typeBox').removeClass("col-xs-10");
+        $('.typeBox').addClass("col-xs-4");
+
+    } else {
+        $('.typeBox').removeClass("col-xs-4");
+        $('.typeBox').addClass("col-xs-10");
+    }
+});
+
