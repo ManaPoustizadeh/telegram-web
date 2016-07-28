fa6eb9493581b5e25dd2eb9c3c3664d090aa5e64
Mana <mana0pst@gmail.com>
2016-07-28 10:23:27 +0430

    chat top menu completed

--- chatJS.js
+++ chatJS.js
@@ -1,3 +1,28 @@
 /**
  * Created by mrolaf on 7/27/16.
  */
+var mediaFlag = false;
+
+
+$('body').click(function () {
+    if(mediaFlag){
+        alert("af");
+        $('#mediaIcon').css("background" , "#4e7492");
+        mediaFlag = false;
+    }
+});
+
+$('#mediaIcon').click(function () {
+    if(!mediaFlag){
+        $('#mediaIcon').css("background" , "#4e7492");
+        mediaFlag = true;
+    }
+    else if(mediaFlag){
+        $('#mediaIcon').css("background" , "#5681a2");
+        mediaFlag = false;
+    }
+    $('#media').toggle();
+});
+
+
+    
